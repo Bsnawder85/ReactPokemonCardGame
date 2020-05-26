4c19d41cd59deb498c7957c0d6c866e1291a16ba
Bobby Snawder <bobbysnawder@bobbys-mbp.lan>
2020-05-26 15:36:31 -0500

    Added Pokedex, Pokecard, with imports.

---
+++ src/Pokecard.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import React from 'react';
+
+class Pokecard extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+
+export default Pokecard;
---
+++ src/Pokedex.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import React from 'react';
+import Pokecard from './Pokecard';
+
+class Pokedex extends Component {
+    render() {
+        return (
+            <div className='Pokedex'></div>
+        );
+    }
+}
+
+export default Pokedex;
